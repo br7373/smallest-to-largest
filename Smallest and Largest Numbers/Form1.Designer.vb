0e533e48402573e5559e5ae40339e48036d61e02
br7373 <46458606+br7373@users.noreply.github.com>
2019-02-25 13:40:49 -0600

    Add files via upload

---
+++ Smallest and Largest Numbers/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblSmall = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblLarge = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(80, 90)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Smallest Number"
+        '
+        'lblSmall
+        '
+        Me.lblSmall.AutoSize = True
+        Me.lblSmall.Location = New System.Drawing.Point(113, 138)
+        Me.lblSmall.Name = "lblSmall"
+        Me.lblSmall.Size = New System.Drawing.Size(10, 13)
+        Me.lblSmall.TabIndex = 1
+        Me.lblSmall.Text = "."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(307, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Largest Number"
+        '
+        'lblLarge
+        '
+        Me.lblLarge.AutoSize = True
+        Me.lblLarge.Location = New System.Drawing.Point(339, 138)
+        Me.lblLarge.Name = "lblLarge"
+        Me.lblLarge.Size = New System.Drawing.Size(10, 13)
+        Me.lblLarge.TabIndex = 3
+        Me.lblLarge.Text = "."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(554, 356)
+        Me.Controls.Add(Me.lblLarge)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblSmall)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblSmall As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblLarge As Label
+End Class
---
+++ Smallest and Largest Numbers/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim input As String
+        Dim small As Integer = 12521112
+        Dim large As Integer
+        Dim num As Integer
+
+        Do
+            Do 'getting input from user
+                input = InputBox("Enter a sequence of positive numbers. When you've finished, enter '0'")
+            Loop While (IsNumeric(input) = False)
+            num = Val(input)
+            If num > input Then
+                large = input
+            End If
+            If num < input Then
+                num = small
+            End If
+            If Val(input) = 0 Then
+                Exit Do
+            End If
+        Loop While num <> 0
+
+        lblSmall.Text = small
+        lblLarge.Text = large
+
+    End Sub
+End Class
